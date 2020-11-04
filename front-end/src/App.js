493e407ab3b1aad1875f3e70196172eb91732516
Madhuri_Sonawat <madhurisonawat@gmail.com>
2020-11-04 18:44:28 +0530

    added topnav and routnav

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Routes from "./routes/Routes.jsx";
+import TopNav from "./components/dashboard/TopNavBar/TopNav";
+import RouteNav from "./components/dashboard/TopNavBar/RouteNav";
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
+      <RouteNav />
       <Routes />{" "}
     </div>
   );
