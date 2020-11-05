30af7abb7da4de3dfae97dadfcf8f33acf89b9e3
RutvikJogdand <jogdandrutvik7@gmail.com>
2020-11-05 15:48:27 +0530

    Updated the styles for the all products page

--- front-end/src/components/CategoriesPageMain/cardContainer/CardContainer.jsx
+++ front-end/src/components/CategoriesPageMain/cardContainer/CardContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CardContainer.module.css";
 import { TiShoppingCart } from "react-icons/ti";
+import Grid from '@material-ui/core/Grid';
 
 function CardContainer(props) {
   const handleClick = () => {
@@ -9,48 +10,54 @@ function CardContainer(props) {
 
   return (
     <>
+        <Grid container >
       {props.foodItemDetails.map((item) => {
         return (
-          <div className={styles.productCard}>
-            <img
-              height="250px"
-              src="https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/b05894c9-bafd-45af-95bf-30fe304d242c_425x425.jpg"
-              alt="fruits and veggies"
-            />
-            <div>
-              <img
-                height="20px"
-                src="https://gnbdevcdn.s3-ap-southeast-1.amazonaws.com/Marketing/8ab57ed4-47e0-426a-8382-f4c89b11826a.png"
-                alt="delivery"
-              />{" "}
-              <span
-                style={{
-                  color: "#880033",
-                  fontSize: "15px",
-                  fontWeight: "600",
-                }}
-              >
-                DELIVERY IN 60 MINUTES{" "}
-              </span>
-            </div>
-            <p> {item.name} </p>
-            <p> {item.qty} </p>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <span className={styles.mrpBorder}>MRP ₹{item.price} </span>
-              <span onClick={handleClick} className={styles.AddBtn}>
-                <TiShoppingCart
-                  style={{
-                    color: "white",
-                    marginRight: "2px",
-                    marginTop: "-2px",
-                  }}
+          <>
+            <Grid container item xs={12} lg={3} md={4} sm={12} >
+                <div className={styles.productCard}>
+                <img
+                  height="250px"
+                  src="https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/b05894c9-bafd-45af-95bf-30fe304d242c_425x425.jpg"
+                  alt="fruits and veggies"
                 />
-                Add
-              </span>
-            </div>
-          </div>
+                <div>
+                  <img
+                    height="20px"
+                    src="https://gnbdevcdn.s3-ap-southeast-1.amazonaws.com/Marketing/8ab57ed4-47e0-426a-8382-f4c89b11826a.png"
+                    alt="delivery"
+                  />{" "}
+                  <span
+                    style={{
+                      color: "#880033",
+                      fontSize: "15px",
+                      fontWeight: "600",
+                    }}
+                  >
+                    DELIVERY IN 60 MINUTES{" "}
+                  </span>
+                </div>
+                <p> {item.name} </p>
+                <p> {item.qty} </p>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <span className={styles.mrpBorder}>MRP ₹{item.price} </span>
+                  <span onClick={handleClick} className={styles.AddBtn}>
+                    <TiShoppingCart
+                      style={{
+                        color: "white",
+                        marginRight: "2px",
+                        marginTop: "-2px",
+                      }}
+                    />
+                    Add
+                  </span>
+                </div>
+              </div>
+            </Grid>
+          </>
         );
       })}
+      </Grid>
     </>
   );
 }
