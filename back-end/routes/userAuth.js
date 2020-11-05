bcb71c2f3fb95381ef6113519092a08dea65e256
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-11-05 21:19:21 +0530

    register otp implemented from backend

--- back-end/routes/userAuth.js
+++ back-end/routes/userAuth.js
@@ -2,10 +2,16 @@ const dotenv = require("dotenv");
 const express = require("express");
 const { body } = require("express-validator");
 
-const { registerUser, loginUser } = require("./../controllers/user-controller");
+const {
+  sendOTP,
+  registerUser,
+  loginUser,
+} = require("./../controllers/user-controller");
 dotenv.config();
 
 const router = express.Router();
+
+router.post("/send-otp", sendOTP);
 
 router.post("/register", [body("email").isEmail()], registerUser);
 
