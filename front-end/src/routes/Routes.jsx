fa5904651be4e1335f79f5ca5cd25c7f748b6a27
Ahmed Raza Khan <khanahmed925@gmail.com>
2020-11-05 13:18:39 +0530

    query params used and try catch test file added

--- front-end/src/routes/Routes.jsx
+++ front-end/src/routes/Routes.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CategoriesPage from "./../components/CategoriesPageMain/categoriesPage/CategoriesPage";
 import Dashboard from "./../components/dashboard/Dashboard";
-
+import Test from "../components/Test"
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" render={() => <Dashboard />} />
       <Route path="/fruits-vegetables" render={() => <CategoriesPage/>} />
+      <Route path="/test" render={() => <Test />} />
     </Switch>
   );
 };
